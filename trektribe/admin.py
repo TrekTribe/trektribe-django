1804397fe537cb92622f6bb7aeeb063d1eb39386
Denny Biasiolli <dennybiasiolli@users.noreply.github.com>
2023-05-03 11:41:01 +0200

    adding migrations and admin for Event

--- trektribe/admin.py
+++ trektribe/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Event
+
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ["title", "date_time", "user"]
+    list_filter = ["title", "date_time"]
