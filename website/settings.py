d3f7ab9021187c3e96c1237107f0db12b57a9135
Denny Biasiolli <dennybiasiolli@users.noreply.github.com>
2023-05-09 14:31:16 +0200

    installing and configuring django-cleanup

--- website/settings.py
+++ website/settings.py
@@ -61,6 +61,7 @@ INSTALLED_APPS = [
     "rest_framework",
     "django_filters",
     "trektribe.apps.TrekTribeConfig",
+    "django_cleanup.apps.CleanupConfig",
 ]
 
 MIDDLEWARE = [
@@ -145,6 +146,7 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / "static"
+MEDIA_ROOT = BASE_DIR / "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
