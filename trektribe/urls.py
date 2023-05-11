4f9037f8869d25f2bc3a2654ff8e66b4a26612f0
Denny Biasiolli <dennybiasiolli@users.noreply.github.com>
2023-05-11 16:33:33 +0200

    adding a basic sitemap with all events

--- trektribe/urls.py
+++ trektribe/urls.py
@@ -1,8 +1,20 @@
+from django.contrib.sitemaps.views import sitemap
 from django.urls import path
 
 from . import views
+from .sitemaps import EventSitemap
 
 urlpatterns = [
     path("", views.event_list, name="event_list"),
     path("event/<int:pk>/", views.event_detail, name="event_detail"),
+    path(
+        "trektribe-sitemap.xml",
+        sitemap,
+        {
+            "sitemaps": {
+                "events": EventSitemap,
+            },
+        },
+        name="django.contrib.sitemaps.views.sitemap",
+    ),
 ]
